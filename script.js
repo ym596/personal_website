457c053d14d6b37eda4533a40814671cc9cca9e5
ym596 <ym596@georgetown.edu>
2025-04-21 19:56:42 -0400

    committing local changes to github

---
+++ script.js
@@ -0,0 +1,7 @@
+// Optional smooth scroll and basic animation on page load
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.fade-in').forEach(element => {
+      element.classList.add('fade-in');
+    });
+  });
+  
